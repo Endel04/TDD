231f8082ea40b2af176b080a8e3d2ca0e420505d
유하람 <w2036@e-mirim.hs.kr>
2021-08-26 19:54:05 +0900

    Add Back And Save Button

---
+++ app/src/main/java/kr/hs/emirim/w2036/tdd/January1.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.w2036.tdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class January1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_january1);
+
+        
+    }
+}
